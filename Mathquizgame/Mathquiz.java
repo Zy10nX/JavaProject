a66e5e84f81bee3aec5c7d73da9ea1cb55fffa31
Zy10nX <izyanamirali@gmail.com>
2024-04-24 13:56:06 +0300

    Mathquiz

--- Mathquizgame/Mathquiz.java
+++ Mathquizgame/Mathquiz.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
 
-public class mathquiz {
+interface Mathquizlook{
+
+    String game = "Izyan-Math-quiz";
+    void formingoptions();
+    void addition();
+    void subtraction();
+    void multiplication();
+    void division();
+
+}
+
+class Userdata implements Mathquizlook{
 
     // decalaring fields.
     public String name;
@@ -9,22 +20,22 @@ public class mathquiz {
     Scanner gameinput = new Scanner(System.in);
 
     // forming constructors
-    public mathquiz(String name) {
+    public Userdata(String name) {
         this.name = name;
     }
+    
     // forming methods.
+    @Override
     public void formingoptions(){
-
-        System.out.println("Welcome to Izyan math quiz game.");
 
         System.out.print("Enter your name: ");
         String name = gameinput.nextLine();
 
         System.out.println("Make a choice for the game play; ");
-        System.out.println("1.Addition");
-        System.out.println("2.Subtraction");
-        System.out.println("3.Multiplication");
-        System.out.println("4.Division");
+        System.out.println("1.Easymode");
+        System.out.println("2.Mediummode");
+        System.out.println("3.Hardmode");
+        System.out.println("4.Extrahard");
         System.out.println("5.Exit");
 
         System.out.print("Enter your choice: ");
@@ -35,13 +46,13 @@ public class mathquiz {
                 addition();
                 break;
             case 2:
-                //subtraction();
+                subtraction();
                 break;
             case 3:
-               // multiplication();
+               multiplication();
                 break;
             case 4:
-               // division();
+               division();
                 break;
             case 5:
                 System.exit(0);
@@ -51,29 +62,145 @@ public class mathquiz {
                 break;
         }
     }
-            // return methods for the game.
+    @Override
+    public void addition(){}
+    @Override
+    public void subtraction(){}
+    @Override
+    public void multiplication(){}
+    @Override
+    public void division(){}
+
+}
+
+class Mathquizz extends Userdata {
+
+    
+    public Mathquizz(String name) {
+        super(name);
+    }
+
+        // return methods for the game.
+        @Override
         public void addition(){
 
         int[][] questions = {{2, 2}, {5, 7}, {10, 3}, {15, 9}, {8, 4}, {12, 6}, {20, 11}, {167, 8}, {25, 13}, {30, 17}};
+        int score = 0;
+        System.out.println (" Addition");
 
-        System.out.println (" Addition Edition!");
-
-        // Iterate through the predefined questions
+        
         for (int i = 0; i < questions.length; i++) {
             int num1 = questions[i][0];
             int num2 = questions[i][1];
             System.out.print((i + 1) + ": What is " + num1 + " + " + num2 + "? " +"\n" + "= ");
             double userAnswer = gameinput.nextDouble();
+
+            
+            double correctanswer = (double) num1 + num2;
+
+            if (userAnswer == correctanswer){
+                System.out.println("Correct!");
+                score++;
+            }
+            else {
+            System.out.println("Incorrect!");
+            }
+            System.out.println("Your score is: " + score + " + " + questions.length);
         }
+        
     }
 
+    @Override
+    public void subtraction() {
+        int[][] questions = {{59, 20}, {9033, 3788}, {159, 7}, {203, 95}, {1233, 400}, {1800, 600}, {25, 11}, {170, 8}, {30, 13}, {35, 17}};
+        int score = 0;
+        System.out.println("Medium");
+
+        for (int i = 0; i < questions.length; i++) {
+            int num1 = questions[i][0];
+            int num2 = questions[i][1];
+            System.out.print((i + 1) + ": What is " + num1 + " - " + num2 + "? " + "\n" + "= ");
+            double userAnswer = gameinput.nextDouble();
+
+            
+            double correctanswer = (double) num1 - num2;
+
+            if (userAnswer == correctanswer){
+                System.out.println("Correct!");
+                score++;
+            }
+            else {
+            System.out.println("Incorrect!");
+            }
+            System.out.println( name + " score is: " + score + " - "  + questions.length);
+        }
+        
+    }
+
+    @Override
+    public void multiplication() {
+        int[][] questions = {{222, 222}, {59, 70}, {109, 33}, {155, 9}, {80, 42}, {121, 6}, {78, 16}, {167, 81}, {259, 138}, {301, 19}};
+        int score = 0;
+        System.out.println("Hard");
+
+        for (int i = 0; i < questions.length; i++) {
+            int num1 = questions[i][0];
+            int num2 = questions[i][1];
+            System.out.print((i + 1) + ": What is " + num1 + " * " + num2 + "? " + "\n" + "= ");
+            double userAnswer = gameinput.nextDouble();
+
+            
+            double correctanswer = (double) num1 * num2;
+
+            if (userAnswer == correctanswer){
+                System.out.println("Correct!");
+                score++;
+            }
+            else {
+            System.out.println("Incorrect!");
+            }
+            System.out.println( name+ " score is: " + score + " * " + questions.length);
+
+        }
+        
+    }
+
+    @Override
+    public void division() {
+        int[][] questions = {{106, 31}, {205, 54}, {30, 22}, {4000, 80}, {505, 5}, {600, 4}, {70, 2}, {80, 3}, {90, 12}, {100, 8}};
+        int score = 0;
+        System.out.println("Exra hard");
+
+        for (int i = 0; i < questions.length; i++) {
+            int num1 = questions[i][0];
+            int num2 = questions[i][1];
+            System.out.print((i + 1) + ": What is " + num1 + " / " + num2 + "? " + "\n" + "= ");
+            double userAnswer = gameinput.nextDouble();
+
+            double correctanswer = (double) num1 / num2;
+
+            if (userAnswer == correctanswer){
+                System.out.println("Correct!");
+                score++;
+            }
+            else {
+            System.out.println("Incorrect!");
+            }
+            System.out.println(name +" score is: " + score + " / "  + questions.length);
+            gameinput.close();
+        }
+      
+    }
+}
+
+public class Mathquiz {
     public static void main(String[] args) {
-        mathquiz game = new mathquiz("");
+
+        Mathquizz game = new Mathquizz("");
+
+        System.out.println("Welcome to  " + Mathquizlook.game);
         game.formingoptions();
 
-        System.out.println("Thank you for choosing Izyan math quiz game.");
+        System.out.println("Thank you for choosing Izyan math quiz game " );
     }
-
-
-
 }
